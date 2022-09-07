697d734ad964efc61e090e703bc24b8a3c2c186b
MikiAsai <mikimaronmoka@gmail.com>
2022-09-07 10:23:49 +0000

    [管理者]コントローラー設定

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,2 +1,5 @@
 class Admin::HomesController < ApplicationController
+ def top
+    @post_movies = PostMovie.all
+ end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,32 @@
 class Admin::UsersController < ApplicationController
+    def index
+    # @users = User.page(params[:page]).per(10)
+    @users = User.find(params[:id])
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+        @user = User.find(params[:id])
+    
+    if  @user.update(user_params)
+        redirect_to admin_user_path(@user.id)
+        flash[:notice] = "会員情報が更新されました。"
+    else
+        render :edit
+    end
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :kana_name, :user_name,:introduction,:is_deleted)
+  end
 end
