cbefeacc44a02d0dc79b7ca14ab27f38cfa06c88
MikiAsai <mikimaronmoka@gmail.com>
2022-09-07 10:28:14 +0000

    [ユーザー]コントローラー調整

--- app/controllers/public/post_movies_controller.rb
+++ app/controllers/public/post_movies_controller.rb
@@ -1,2 +1,33 @@
 class Public::PostMoviesController < ApplicationController
+    def index
+   
+    @post_movies = PostMovie.all
+
+    if params[:situation_id] == nil
+    @post_movies = PostMovie.page(params[:page]).per(10)
+    else
+    post_movies = PostMovie.where(situation_id: params[:situation_id])
+    @post_movies = post_movies.page(params[:page]).per(10)
+    @situation = Situation.find(params[:situation_id])
+    end
+    
+   end
+   
+   def show
+    @post_movies = PostMovie.find(params[:id])
+    @post_comments = post_movie.post_comments.all
+    @user = current_user
+   end
+   
+   def create
+    @situations = Situation.all
+    @post_movie = PostMovie.new(post_movie_params)
+    @post_movie.save
+    redirect_to post_movie_path(@post_movie.id)
+   end
+   
+   def post_movie_params
+      params.require(:post_movie).permit(:title, :story, :review, :star, :site)
+   end
+   
 end
