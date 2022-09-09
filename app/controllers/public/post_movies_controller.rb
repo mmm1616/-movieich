3b53ffc97ce0301b3c0f18e91ce4d4e5ce76dd75
MikiAsai <mikimaronmoka@gmail.com>
2022-09-09 01:01:41 +0000

    [ユーザー]投稿編集

--- app/controllers/public/post_movies_controller.rb
+++ app/controllers/public/post_movies_controller.rb
@@ -1,35 +1,50 @@
 class Public::PostMoviesController < ApplicationController
    def new
-      @post_movie = PostMovie.new
-      @situation = Situation.all
+        @post_movie = PostMovie.new
+        @situation = Situation.all
    end
     
    def index
    
-    @post_movies = PostMovie.all
+        @post_movies = PostMovie.all
 
     if params[:situation_id] == nil
-    @post_movies = PostMovie.page(params[:page]).per(10)
+        @post_movies = PostMovie.page(params[:page]).per(10)
     else
-    post_movies = PostMovie.where(situation_id: params[:situation_id])
-    @post_movies = post_movies.page(params[:page]).per(10)
-    @situation = Situation.find(params[:situation_id])
+        post_movies = PostMovie.where(situation_id: params[:situation_id])
+        @post_movies = post_movies.page(params[:page]).per(10)
+        @situation = Situation.find(params[:situation_id])
     end
     
    end
    
    def show
-    @post_movie = PostMovie.find(params[:id])
-    @situation = Situation.find(params[:id])
-    @post_comment = PostComment.new
-    @user = current_user
+        @post_movie = PostMovie.find(params[:id])
+        @situation = Situation.find(params[:id])
+        @post_comment = PostComment.new
+        @user = current_user
    end
    
+    def edit
+        @post_movie = PostMovie.find(params[:id])
+    end
+    
+    def update
+        @post_movie = PostMovie.find(params[:id])
+    
+        if  @post_movie.update(post_movie_params)
+            redirect_to post_movie_path(@post_movie.id)
+            flash[:notice] = "投稿内容が更新されました。"
+        else
+            render :edit
+        end
+    end
+   
    def create
-    @post_movie = PostMovie.new(post_movie_params)
-    @post_movie.user_id = current_user.id
-    @post_movie.save
-    redirect_to post_movie_path(@post_movie.id)
+        @post_movie = PostMovie.new(post_movie_params)
+        @post_movie.user_id = current_user.id
+        @post_movie.save
+        redirect_to post_movie_path(@post_movie.id)
    end
    
    private
