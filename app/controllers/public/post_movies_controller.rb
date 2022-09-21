7a7902168674328d56903d432ad84d88af6c7a14
MikiAsai <mikimaronmoka@gmail.com>
2022-09-21 02:10:05 +0000

    [ユーザー]ページネーション調整

--- app/controllers/public/post_movies_controller.rb
+++ app/controllers/public/post_movies_controller.rb
@@ -10,10 +10,10 @@ class Public::PostMoviesController < ApplicationController
        @post_movies = PostMovie.all
 
     if params[:situation_id] == nil
-        @post_movies = PostMovie.page(params[:page]).per(10)
+        @post_movies = PostMovie.page(params[:page]).per(9)
     else
         post_movies = PostMovie.where(situation_id: params[:situation_id])
-        @post_movies = post_movies.page(params[:page]).per(10)
+        @post_movies = post_movies.page(params[:page]).per(9)
         @situation = Situation.find(params[:situation_id])
     end
     
