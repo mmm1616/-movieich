5d03e65c85c7bf1e074e2ac76fa6d442cf8bda3e
MikiAsai <mikimaronmoka@gmail.com>
2022-09-29 00:06:59 +0000

    検索エラー修正

--- app/controllers/public/post_movies_controller.rb
+++ app/controllers/public/post_movies_controller.rb
@@ -18,10 +18,11 @@ class Public::PostMoviesController < ApplicationController
     end
     
     def search
-        
         if params[:keyword] != ""   
          post_movies = PostMovie.where("title LIKE?", "%#{params[:keyword]}%")
          @post_movies = post_movies.page(params[:page]).per(10)
+        else
+         render :index
         end
     end
     
